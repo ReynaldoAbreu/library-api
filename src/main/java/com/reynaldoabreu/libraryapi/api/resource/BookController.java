65c484b85e897964a01f67edf7c810fcfbccffed
Renaldo Abreu <renaldoc@ciandt.com>
2023-01-10 11:10:37 -0300

    testes de filtar um livro

--- src/main/java/com/reynaldoabreu/libraryapi/api/resource/BookController.java
+++ src/main/java/com/reynaldoabreu/libraryapi/api/resource/BookController.java
@@ -7,11 +7,17 @@ import com.reynaldoabreu.libraryapi.model.entity.Book;
 import com.reynaldoabreu.libraryapi.service.BookService;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -58,6 +64,20 @@ public class BookController {
         service.update(book);
 
         return modelMapper.map(book, BookDTO.class);
+
+    }
+
+    @GetMapping
+    public Page<BookDTO> find(BookDTO dto, Pageable pageRequest){
+
+        Book filter = modelMapper.map(dto, Book.class);
+        Page<Book> result = service.find(filter, pageRequest);
+        List<BookDTO> list = result.getContent()
+                .stream()
+                .map(entity -> modelMapper.map(entity, BookDTO.class))
+                .collect(Collectors.toList());
+
+        return new PageImpl<BookDTO>(list, pageRequest, result.getTotalElements());
 
     }
 
--- src/main/java/com/reynaldoabreu/libraryapi/service/imp/BookServiceImp.java
+++ src/main/java/com/reynaldoabreu/libraryapi/service/imp/BookServiceImp.java
@@ -4,6 +4,10 @@ import com.reynaldoabreu.libraryapi.exception.BusinessException;
 import com.reynaldoabreu.libraryapi.model.entity.Book;
 import com.reynaldoabreu.libraryapi.model.repository.BookRepository;
 import com.reynaldoabreu.libraryapi.service.BookService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -50,6 +54,20 @@ public class BookServiceImp implements BookService {
 
         return this.repository.save(book);
 
+    }
+
+    @Override
+    public Page<Book> find(Book filter, Pageable pageRequest) {
+
+
+        Example<Book> example = Example.of(filter,
+                ExampleMatcher
+                        .matching()
+                        .withIgnoreCase()
+                        .withIgnoreNullValues()
+                        .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING));
+
+        return repository.findAll(example, pageRequest);
     }
 
 }
